06cb8f9b3adc70cc3eea2aa2078fe8ad09a18f89
Mabrur <mabrur.h11@gmail.com>
2022-06-02 18:04:17 +0500

    add search component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
-import { Navbar, Container, Nav, Row, Col } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
+import "./style.css"
 
 export default function HeaderComponent() {
     return (
         <div className="shadow">
             <Container fluid>
-                <Row className="py-3 px-1">
+                <Row className="py-3 px-1 justify-content-between">
                     <Col md={4} className="text-start">Filters</Col>
-                    <Col md={{ span: 4, offset: 4 }} className="text-end">Smth</Col>
+                    <Col md={2}>
+                        <div className="input-group input-group-sm">
+                            <input type="text" className="form-control header-search" aria-label="Sizing example input" placeholder="Search Games..." />
+                        </div>
+                    </Col>
+                    <Col md={4} className="text-end">Smth</Col>
                 </Row>
             </Container>
         </div>
